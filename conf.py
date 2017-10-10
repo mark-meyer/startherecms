b00f6d32bb12aba48bf0fc07b814b7f80b20eb01
Adrienne <adrienne@codeforamerica.org>
2017-10-10 15:41:28 -0700

    moved conf setup to root

--- conf.py
+++ conf.py
@@ -34,7 +34,7 @@
 extensions = ['sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['docs/_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -43,7 +43,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'docs/index'
 
 # General information about the project.
 project = 'Start Here CMS'
@@ -94,7 +94,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['docs/_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
