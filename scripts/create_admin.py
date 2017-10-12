15733620c6043b28832dc7f9b0bddd868ff8ba86
Adrienne <adrienne@codeforamerica.org>
2017-10-12 10:55:22 -0700

    added back syncdb

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -10,5 +10,5 @@ if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
     user.save()
 
 if "DATABASE_URL" in os.environ:
-    os.environ['ADMIN_PASSWORD'] = 'database'
     subprocess.call(["heroku pg:backups:restore 'https://s3.us-east-2.amazonaws.com/database-dump-starthere/c5de9726-b709-4fd9-af32-e03fe390df16' DATABASE_URL;"])
+    print("DB exists")
