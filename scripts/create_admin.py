8dacfd0e008b7926eeaf746a48fa398c3a574239
Adrienne <adrienne@codeforamerica.org>
2017-10-12 10:57:53 -0700

    reverted back to old app json

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -8,7 +8,3 @@ if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
     user.is_superuser=True
     user.is_staff=True
     user.save()
-
-if "DATABASE_URL" in os.environ:
-    subprocess.call(["heroku pg:backups:restore 'https://s3.us-east-2.amazonaws.com/database-dump-starthere/c5de9726-b709-4fd9-af32-e03fe390df16' DATABASE_URL;"])
-    print("DB exists")
