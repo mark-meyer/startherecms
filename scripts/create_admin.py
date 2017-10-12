78c61803640e1d8c7d894443e81018a5c38f5e00
Adrienne <adrienne@codeforamerica.org>
2017-10-12 11:58:01 -0700

    tried using heroku pg again

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -2,7 +2,7 @@
 import os
 import subprocess
 
-subprocess.call("pg_dump -Fc 'postgres://jxpyiubojggyqx:6bb7a6cfb97af5e9ba749c0a83f7dcb6cff147b3b0647b005cabb115bc82854b@ec2-54-83-26-65.compute-1.amazonaws.com:5432/d98a74fiqpdm8s' | pg_restore --clean --no-owner -n public -d $DATABASE_URL && python manage.py migrate", shell=True)
+subprocess.call("heroku pg:backups:restore 'https://s3.us-east-2.amazonaws.com/database-dump-starthere/c5de9726-b709-4fd9-af32-e03fe390df16' DATABASE_URL", shell=True)
 
 if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
     from django.contrib.auth.models import User
