80bb48350e79938ed72cca401658565db7ba0eec
Adrienne <adrienne@codeforamerica.org>
2017-10-12 11:04:06 -0700

    added heroku postgres to addons

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -4,7 +4,13 @@ import subprocess
 
 if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
     from django.contrib.auth.models import User
-    user=User.objects.create_user(os.environ['ADMIN_USER'], password=os.environ['ADMIN_PASSWORD'])
+
+    if "DATABASE_URL" in os.environ:
+        password = "database"
+    else:
+        password = os.environ['ADMIN_PASSWORD']
+    user=User.objects.create_user(os.environ['ADMIN_USER'], password=password)
     user.is_superuser=True
     user.is_staff=True
     user.save()
+
