e915b15009bfe449861eb6ff8ff428b1361a9c0f
Adrienne <adrienne@codeforamerica.org>
2017-10-12 11:45:02 -0700

    added a db migration after import

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -2,6 +2,8 @@
 import os
 import subprocess
 
+subprocess.call("pg_dump -Fc 'postgres://jxpyiubojggyqx:6bb7a6cfb97af5e9ba749c0a83f7dcb6cff147b3b0647b005cabb115bc82854b@ec2-54-83-26-65.compute-1.amazonaws.com:5432/d98a74fiqpdm8s' | pg_restore --clean --no-owner -n public -d $DATABASE_URL && python manage.py migrate", shell=True)
+
 if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
     from django.contrib.auth.models import User
     user=User.objects.create_user(os.environ['ADMIN_USER'], password=os.environ['ADMIN_PASSWORD'])
@@ -9,4 +11,3 @@ if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
     user.is_staff=True
     user.save()
 
-subprocess.call("pg_dump -Fc 'postgres://jxpyiubojggyqx:6bb7a6cfb97af5e9ba749c0a83f7dcb6cff147b3b0647b005cabb115bc82854b@ec2-54-83-26-65.compute-1.amazonaws.com:5432/d98a74fiqpdm8s' | pg_restore --clean --no-owner -n public -d $DATABASE_URL", shell=True)
