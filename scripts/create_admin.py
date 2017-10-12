0d2fdca1e5c5993acf4d214ffb6e2affbf490e86
Adrienne <adrienne@codeforamerica.org>
2017-10-12 10:49:44 -0700

    moved heroku pg out of app json

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -1,5 +1,6 @@
 # create_admin.py
 import os
+import subprocess
 
 if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
     from django.contrib.auth.models import User
@@ -10,3 +11,4 @@ if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
 
 if "DATABASE_URL" in os.environ:
     os.environ['ADMIN_PASSWORD'] = 'database'
+    subprocess.call(["heroku pg:backups:restore 'https://s3.us-east-2.amazonaws.com/database-dump-starthere/c5de9726-b709-4fd9-af32-e03fe390df16' DATABASE_URL;"])
