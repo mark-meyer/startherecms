b45e63618a24aa7ea477acd8ef3dc673db7d2b34
Adrienne <adrienne@codeforamerica.org>
2017-10-12 11:27:08 -0700

    used pg_restore instead of heroku

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -4,13 +4,9 @@ import subprocess
 
 if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
     from django.contrib.auth.models import User
-
-    if "DATABASE_URL" in os.environ:
-        password = "database"
-    else:
-        password = os.environ['ADMIN_PASSWORD']
-    user=User.objects.create_user(os.environ['ADMIN_USER'], password=password)
+    user=User.objects.create_user(os.environ['ADMIN_USER'], password=os.environ['ADMIN_PASSWORD'])
     user.is_superuser=True
     user.is_staff=True
     user.save()
 
+subprocess.call("pg_restore --clean --no-owner -n public -d $DATABASE_URL https://s3.us-east-2.amazonaws.com/database-dump-starthere/c5de9726-b709-4fd9-af32-e03fe390df16", shell=True)
